13c34eacaf82182d19571f6afbcaf2f021c99878
Ravidu Dilshan <36620171+RavinduDilshan@users.noreply.github.com>
2024-12-12 10:48:55 +0530

    theme provider file added and .g file generated

---
+++ lib/theme_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'theme_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$isLightThemeHash() => r'9aa9e9a2c5665e51853add4892ca0dab751d3bab';
+
+/// See also [IsLightTheme].
+@ProviderFor(IsLightTheme)
+final isLightThemeProvider =
+    AutoDisposeNotifierProvider<IsLightTheme, bool>.internal(
+  IsLightTheme.new,
+  name: r'isLightThemeProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$isLightThemeHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$IsLightTheme = AutoDisposeNotifier<bool>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
